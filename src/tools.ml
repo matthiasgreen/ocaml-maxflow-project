e4ac079c695207a25f9ab250fe38dc2bfa1e5734
Green Matthias <mgreen@insa-11291>
2024-12-05 10:45:19 +0100

    Add add_arc tool and rename gmap_arcs

--- src/tools.ml
+++ src/tools.ml
@@ -7,8 +7,18 @@ let gmap graph fm =
     e_fold graph (fun acu arc -> new_arc acu {src=arc.src;tgt=arc.tgt;lbl=fm arc.lbl}) (clone_nodes graph)
     
 
-let better_gmap graph fm =
+let gmap_arcs graph fm =
     e_fold graph (fun acu arc -> new_arc acu (fm arc)) (clone_nodes graph)
 
 
-(* let add_arc: int graph -> id -> id -> int -> int graph = failwith "not implemented" *)
+let add_arc gr src tgt n =
+    let possible_arc = find_arc gr src tgt in
+    match possible_arc with
+    | None -> (new_arc gr {src; tgt; lbl=n})
+    | Some _ ->
+        gmap_arcs gr (
+            function
+            | a when a.src = src && a.tgt = tgt -> {a with lbl=a.lbl+n} 
+            | a -> a
+        )
+;;
